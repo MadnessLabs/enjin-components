dfdbac15a7df43f5528eef03f286fc735bb286fe
Popcorn245 <Bobby@madnesslabs.net>
2019-03-09 16:36:57 -0600

    Formatting

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -1,12 +1,11 @@
-import { Component, Listen } from '@stencil/core';
+import { Component, Listen } from "@stencil/core";
 
 @Component({
-  tag: 'enjin-gallery',
-  styleUrl: 'gallery.css'
+  tag: "enjin-gallery",
+  styleUrl: "gallery.css"
 })
 export class Gallery {
-
-  @Listen('enjinStarRating')
+  @Listen("enjinStarRating")
   onStarRating(event) {
     console.log(event);
   }
@@ -18,41 +17,42 @@ export class Gallery {
         description="A Stripe Elements card component"
         propList={[
           {
-            name: 'stripeKey',
-            description: 'The publishable Stripe key for the account you want to use'
+            name: "stripeKey",
+            description:
+              "The publishable Stripe key for the account you want to use"
           }
         ]}
       >
         <enjin-pay-with-card stripeKey="pk_test_G6ksY0dKXlgogvnitD0Wm1oc" />
-      </enjin-organism>, 
+      </enjin-organism>,
       <enjin-organism
         name="Star Rating"
         description="A simple 5 star rating component"
         propList={[]}
       >
         <enjin-star-rating />
-      </enjin-organism>, 
+      </enjin-organism>,
       <enjin-organism
-      name="Star Rating Scoped"
-      description="A simple 5 star rating component"
-      propList={[]}
-    >
-      <enjin-star-rating-scoped />
-    </enjin-organism>, 
-    <enjin-organism
-    name="Star Rating Shadow"
-    description="A simple 5 star rating component"
-    propList={[]}
-  >
-    <enjin-star-rating-shadow />
-  </enjin-organism>, 
+        name="Star Rating Scoped"
+        description="A simple 5 star rating component"
+        propList={[]}
+      >
+        <enjin-star-rating-scoped />
+      </enjin-organism>,
+      <enjin-organism
+        name="Star Rating Shadow"
+        description="A simple 5 star rating component"
+        propList={[]}
+      >
+        <enjin-star-rating-shadow />
+      </enjin-organism>,
       <enjin-organism
         name="Test Component"
         description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         propList={[
           {
-            name: 'testOne',
-            description: 'A test prop to do nothing'
+            name: "testOne",
+            description: "A test prop to do nothing"
           }
         ]}
       >
@@ -63,8 +63,8 @@ export class Gallery {
         description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         propList={[
           {
-            name: 'testTwo',
-            description: 'A test prop to do nothing'
+            name: "testTwo",
+            description: "A test prop to do nothing"
           }
         ]}
       >
@@ -75,8 +75,8 @@ export class Gallery {
         description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         propList={[
           {
-            name: 'testTwo',
-            description: 'A test prop to do nothing'
+            name: "testTwo",
+            description: "A test prop to do nothing"
           }
         ]}
       >
@@ -87,8 +87,8 @@ export class Gallery {
         description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         propList={[
           {
-            name: 'testTwo',
-            description: 'A test prop to do nothing'
+            name: "testTwo",
+            description: "A test prop to do nothing"
           }
         ]}
       >
