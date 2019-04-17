2e638ac3f00c80b802c1043eeb0f80fc7a15edbe
Bobby Johnson <Bobby@MadnessLabs.net>
2019-04-17 00:38:36 -0500

    Add another phase example

--- src/components/star-rating/star-rating.phases.ts
+++ src/components/star-rating/star-rating.phases.ts
@@ -1,8 +1,15 @@
-export default {
+export default  {
     test: {
         name: "with 10 star max",
         props: {
             maxRating: 10
+        }
+    },
+    disabled: {
+        props: {
+            maxRating: 10,
+            value: 5,
+            disabled: true 
         }
     }
 };
