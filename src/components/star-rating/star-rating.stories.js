44082bce736253e2b2e3b3c10dcc9d2351c00df4
Bobby Johnson <Bobby@MadnessLabs.net>
2020-02-11 11:43:47 -0600

    fix(): Implement Storybook knobs example

--- src/components/star-rating/star-rating.stories.js
+++ src/components/star-rating/star-rating.stories.js
@@ -1,33 +1,29 @@
 import { storiesOf } from '@storybook/html';
 import { withActions } from "@storybook/addon-actions";
+import { withKnobs, number, boolean } from "@storybook/addon-knobs";
 
 import readme from "./readme.md";
 
+const storyOptions = {
+  notes: {
+    markdown: readme
+  }
+};
+
 storiesOf('Star Rating', module)
   .addDecorator(withActions("enjinStarRating"))
+  .addDecorator(withKnobs)
   .addParameters({ jest: ["star-rating"] })
-  .add('Default', () => '<enjin-star-rating></enjin-star-rating>', {
-    notes: {
-      markdown: readme
-    }
-  })
+  .add('Default', () => '<enjin-star-rating></enjin-star-rating>', storyOptions)
   .add('With Rating', () => {
     const starRatingEl = document.createElement('enjin-star-rating');
-    starRatingEl.value = 3;
+    starRatingEl.value = number('value', 3);
 
     return starRatingEl;
-  }, {
-    notes: {
-      markdown: readme
-    }
-  })
+  }, storyOptions)
   .add('Disabled', () => {
     const starRatingEl = document.createElement('enjin-star-rating');
-    starRatingEl.disabled = true;
+    starRatingEl.disabled = boolean('disabled', true);
 
     return starRatingEl;
-  }, {
-    notes: {
-      markdown: readme
-    }
-  });
+  }, storyOptions);
